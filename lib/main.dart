f81268b99f1916195a3b44e363bed0747d3abd43
Jlopezscala <julian.scala@electric.ai>
2020-09-06 20:47:23 -0300

    Add comments on First Lesson -> main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,25 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+
+  // This is a Stateful Widget, createState method needs to be overwritten
+  // The widget needs to go along a State child class sort of '{widget_name}State'
+  // In order to simply change the state, setState((), {a function, or steps}) needs
+  // To be called and the Widget will redraw itself.
+
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
+// Underscore indicates private (code style)
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
-
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
   }
 
@@ -33,18 +39,18 @@ class MyAppState extends State<MyApp> {
             ),
             body: Column(
               children: <Widget>[
-                Text(questions[questionIndex]),
+                Text(questions[_questionIndex]),
                 RaisedButton(
                   child: Text("Answer 1"),
-                  onPressed: answerQuestion,
+                  onPressed: _answerQuestion,
                 ),
                 RaisedButton(
                   child: Text("Answer 2"),
-                  onPressed: answerQuestion,
+                  onPressed: _answerQuestion,
                 ),
                 RaisedButton(
                   child: Text("Answer 3"),
-                  onPressed: answerQuestion,
+                  onPressed: _answerQuestion,
                 ),
               ],
             )));
